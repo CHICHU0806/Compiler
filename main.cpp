0f3ea0c457206b87bea345dca1df83dc7e4e9b3c
CHICHU <2085219596@qq.com>
2026-05-08 08:50:27 +0800

    5.7

--- main.cpp
+++ main.cpp
@@ -28,6 +28,36 @@ public:
 
     Token nextToken() {
 
+        skipWhitespace();
+
+        if (pos_ >= input_.size()) {
+            return {TokenType::End, ""};
+        }
+
+        char ch = input_[pos_];
+
+        if (std::isdigit(static_cast<unsigned char>(ch))) {
+            return readNumber();
+        }
+
+        pos_++;
+
+        switch (ch) {
+            case '+':
+                return {TokenType::Plus, ""};
+            case '-':
+                return {TokenType::Minus, ""};
+            case '*':
+                return {TokenType::Mul, ""};
+            case '/':
+                return {TokenType::Div, ""};
+            case '(':
+                return {TokenType::LParen, ""};
+            case ')':
+                return {TokenType::RParen, ""};
+            default:
+                return {TokenType::Invalid, std::string(1, ch)};
+        }
     }
 
 
@@ -37,6 +67,21 @@ private:
 
     //私有函数：跳过空格
     void skipWhitespace() {
-        while (pos_ < input_.size() && std ::isspace(input_[pos_])) {}
+        //遍历过程中完成
+        while (pos_ < input_.size() && std::isspace(static_cast<unsigned char>(input_[pos_]))){
+            pos_++;
+        }
+    }
+
+    //读数字
+    //但是其目的是为了在遍历过程中实现
+    Token readNumber() {
+        size_t start = pos_;
+
+        while (pos_ < input_.size() && std::isdigit(static_cast<unsigned char>(input_[pos_]))) {
+            pos_++;
+        }
+
+        return {TokenType::Number,input_.substr(start,pos_-start)};
     }
 };
